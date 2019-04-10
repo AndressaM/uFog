574f110747762da891e4265ad91e2c1c4b22a25a
Andressa Martins <andressamartins@ic.ufal.br>
2019-04-10 00:14:36 -0300

    add functions onLed && fadeLed

--- Módulo Wifi/nodeFirebase.ino
+++ Módulo Wifi/nodeFirebase.ino
@@ -1,34 +1,18 @@
-//
-// Copyright 2015 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-// FirebaseStream_ESP8266 is a sample that stream bitcoin price from a
-// public Firebase and optionally display them on a OLED i2c screen.
 
 #include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
 
 
 // Set these to run example.
-#define WIFI_SSID "IEEERAS"
-#define WIFI_PASSWORD "soolucassabe"
+#define WIFI_SSID "gil"
+#define WIFI_PASSWORD "chico1718"
 
 #define FIREBASE_HOST "ufog-a1af1.firebaseio.com"
 #define FIREBASE_AUTH "sOb85BW72hephLNGtxITVY3qptEX1wWB5yGPmkh0"
 
 int led = 0;
+double brightness = 0.0;
+double fade = 0.0;
 
 void setup() {
   Serial.begin(9600);
@@ -45,7 +29,8 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.stream("/lamp");  
+  Firebase.stream("/lamp");
+    
 }
 
 
@@ -62,7 +47,8 @@ void loop() {
      eventType.toLowerCase();
 
     
-    led = Firebase.getBool("lamp");
+    led = Firebase.getBool("/lamp");
+    fade = Firebase.getInt("/fade");
      
      Serial.print("event: ");
      Serial.println(eventType);
@@ -71,19 +57,31 @@ void loop() {
        Serial.println(event.getString("data"));
        String path = event.getString("path");
        String data = event.getString("data");
+       onLed(2,led);
+       if(led == 1)
+       {
+        fadeLed(2, fade);
+       }
      }
 
   }   
+   
     
-    
-    if(led == 0){
-      digitalWrite(2 ,HIGH);
-      //Serial.print("led: ");
-      //Serial.println(led);
+
+}
+
+void onLed(int led , int ledstatus){
+    if(ledstatus == 0){
+      digitalWrite(led ,HIGH);
     }
-    else if(led == 1){
-      digitalWrite(2 ,LOW);
-      //Serial.print("led: ");
-      //Serial.println(led);
+    else if(ledstatus == 1){
+      digitalWrite(led ,LOW);
     }
 }
+
+void fadeLed(int led , int pcntLed)
+{
+  brightness = 255*(pcntLed)/100.0; 
+  analogWrite(led, brightness); 
+}
+
